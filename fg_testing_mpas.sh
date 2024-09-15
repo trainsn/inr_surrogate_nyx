78529887d21de4dee73dc69e88da56d0839c2eed
trainsn <shineng9664@gmail.com>
2024-09-15 18:38:37 -0400

    add Evidential loss

--- fg_testing_mpas.sh
+++ fg_testing_mpas.sh
@@ -4,4 +4,4 @@
 #PBS -l nodes=1:ppn=1:gpus=1
 #PBS -l mem=48GB
 
-python3 -u fg_testing_mpas.py --root /fs/ess/PAS0027/mpas_graph/ --dir-weights /fs/ess/PAS0027/mpas_inr/model_weights/ --dir-outputs /fs/ess/PAS0027/mpas_inr/outputs/ --batch-size 1974191 --log-every 1 --check-every 2 --start-epoch 35 --loss Evidential --dim3d 64 --dim2d 256 --dim1d 16 --spatial-fdim 32 --param-fdim 16 --dropout 0
+python3 -u fg_testing_mpas.py --root /fs/ess/PAS0027/mpas_graph/ --dir-weights /fs/ess/PAS0027/mpas_inr/model_weights/ --dir-outputs /fs/ess/PAS0027/mpas_inr/outputs/ --batch-size 1974191 --log-every 1 --check-every 2 --start-epoch 70 --loss Evidential --dim3d 64 --dim2d 256 --dim1d 16 --spatial-fdim 32 --param-fdim 16 --dropout 0
--- fg_training_impsamp_ensemble.sh
+++ fg_training_impsamp_ensemble.sh
@@ -1,7 +1,7 @@
 #!bin/sh
-#PBS -N INRSurro_Nyx_MSE
+#PBS -N INRSurro_Nyx_Evidential
 #PBS -l walltime=36:00:00
 #PBS -l nodes=1:ppn=1:gpus=1
 #PBS -l mem=48GB
 
-python3 -u fg_training_impsamp_ensemble.py --root /fs/ess/PAS0027/nyx_vdl/512/train/ --dir-weights /fs/ess/PAS0027/nyx_inr/model_weights/ --dir-outputs /fs/ess/PAS0027/nyx_inr/outputs/ --batch-size 262144 --sf-sr 0.10 --log-every 1 --check-every 2 --start-epoch 0 --epochs 500 --loss MSE --dim3d 64 --dim2d 256 --dim1d 16 --spatial-fdim 32 --param-fdim 16
+python3 -u fg_training_impsamp_ensemble.py --root /fs/ess/PAS0027/nyx_vdl/512/train/ --dir-weights /fs/ess/PAS0027/nyx_inr/model_weights/ --dir-outputs /fs/ess/PAS0027/nyx_inr/outputs/ --batch-size 262144 --sf-sr 0.10 --log-every 1 --check-every 2 --start-epoch 0 --epochs 500 --loss Evidential --dim3d 64 --dim2d 256 --dim1d 16 --spatial-fdim 32 --param-fdim 16
